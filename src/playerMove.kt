025bb00fb3f3634166224880cb8d8905f13637f6
Julian <167174811+JBTastic@users.noreply.github.com>
2025-05-24 23:09:47 +0200

    fix: Make user input to lowercase

--- src/playerMove.kt
+++ src/playerMove.kt
@@ -1,5 +1,5 @@
 // returns true if move was valid, otherwise returns false
-fun playerMove(): Boolean {
+fun playerMove(wordList: Array<String>): Boolean {
     println("Enter your guess!")
 
     val input = readLine()
@@ -7,11 +7,11 @@ fun playerMove(): Boolean {
     if (input.isNullOrEmpty() || input.length != 5) {
         println("Enter a 5 letter word!")
         return false
-    } else if (input !in wordList) {
+    } else if (input.lowercase() !in wordList) {
         println ("Enter a valid word!")
         return false
     } else {
-        inputWord = input
+        inputWord = input.lowercase()
         return true
     }
 }
