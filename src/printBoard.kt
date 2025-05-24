b581dc8c594624ad2d1c0e3187e94f44f3648615
Julian <167174811+JBTastic@users.noreply.github.com>
2025-05-24 23:10:36 +0200

    feat: Board now has color depending on the correctness of the word

--- src/printBoard.kt
+++ src/printBoard.kt
@@ -1,14 +1,20 @@
 // 5 columns X 6 rows
 
 
-fun printBoard() {
+fun printBoard(language: Int?, wantedWord: String) {
+    println("The word were are looking for is in ${languages[language]}!")
+    println("Wanted word is $wantedWord")
     val horizontalLine = " "+"-".repeat(19)
 
     println(horizontalLine)
     for (i in 0 until rows) {
         print("|")
         for (j in 0 until cols) {
-            print(" ${board[i][j]} |")
+            when {
+                board[i][j].lowercase() == wantedWord[j].lowercase() -> print(" $green${board[i][j]}$reset |")
+                board[i][j].lowercase() in wantedWord.lowercase() -> print(" $yellow${board[i][j]}$reset |")
+                else -> print(" $gray${board[i][j]}$reset |")
+            }
         }
     println()
     println(horizontalLine)
