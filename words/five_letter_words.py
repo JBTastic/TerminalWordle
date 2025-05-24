c76abeabc3cca05f1a06d41aabe015d71b99508f
Julian <167174811+JBTastic@users.noreply.github.com>
2025-05-25 00:36:19 +0200

    refactor: changed input and output files in one of the python scripts

--- words/five_letter_words.py
+++ words/five_letter_words.py
@@ -1,7 +1,7 @@
 # filter_five_letter_words.py
 
-input_file = "simple_wordlist.txt"          # Ursprüngliche Datei mit allen Wörtern
-output_file = "simple_words_5.txt"       # Neue Datei mit nur 5-Buchstaben-Wörtern
+input_file = "german_words.txt"          # Ursprüngliche Datei mit allen Wörtern
+output_file = "german_words_5.txt"       # Neue Datei mit nur 5-Buchstaben-Wörtern
 
 with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
     for line in infile:
